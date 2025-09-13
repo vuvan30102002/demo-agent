8af8045ccc659fb662d4780d6b504c668251bdf6
quang vux <quangvanvu312@gmail.com>
2025-09-13 12:11:32 +0700

    13september

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 1. Cài thư viện bao gồm có:
 + Thư viện để tải model embedding: sentence-transformers, ctransformers, transformers
 + Thư viện để đọc file PDF, lưu vecter DB, bảo bật env, sử dụng huggingface : langchain, langchain-community, torch, pypdf, gpt4all, faiss-cpu, openai, PyPDF2, langchain-huggingface, python-dotenv
-+ Thư viện phục vụ sử dụng Fast API và kết nối csdl: chromadb, pyodbc, sqlmodel, fastapi, python-multipart, typing, pydantic
++ Thư viện phục vụ sử dụng Fast API và kết nối csdl: mysql-connector-python, fastapi, python-multipart, typing, pydantic
 + Thư viện để sử dụng session: itsdangerous
--- connectData.py
+++ connectData.py
@@ -1,26 +1,35 @@
-import pyodbc
-import pandas as pd
-server = "DESKTOP-2O4N45Q\\SQLEXPRESS"
-database = "Agent"
-username = "quang"
-password = "30102002"
-connection_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
-# try:
-#     conn = pyodbc.connect(connection_string)
-#     # query = "SELECT * FROM SinhVien"
-#     # df_account = pd.read_sql(query,conn)
-#     # print(df_account.head())
-# except pyodbc.Error as e:
-#     print(e)
-# finally:
-#     if conn:
-#         conn.close()
+# import pyodbc
+# import pandas as pd
+# server = "DESKTOP-2O4N45Q\\SQLEXPRESS"
+# database = "Agent"
+# username = "quang"
+# password = "30102002"
+# connection_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+# def get_connect():
+#     try:
+#         conn = pyodbc.connect(connection_string)
+#         return conn
+#     except pyodbc.Error as e:
+#         print(e)
+
+import mysql.connector
+
+# Thông tin kết nối
+config = {
+    "host": "localhost",     # Địa chỉ server MySQL (VD: "127.0.0.1")
+    "user": "root",          # Tên user MySQL
+    "password": "",  # Mật khẩu user MySQL
+    "database": "agent_promotion"   # Tên database cần kết nối
+}
 def get_connect():
     try:
-        conn = pyodbc.connect(connection_string)
-        # query = "SELECT * FROM SinhVien"
-        # df_account = pd.read_sql(query,conn)
-        # print(df_account.head())
+        # Kết nối tới MySQL
+        conn = mysql.connector.connect(**config)
         return conn
-    except pyodbc.Error as e:
-        print(e)
+    except mysql.connector.Error as err:
+        print(f"Lỗi: {err}")
+
+    finally:
+        # Đóng kết nối
+        if 'conn' in locals() and conn:
+            conn.close()
